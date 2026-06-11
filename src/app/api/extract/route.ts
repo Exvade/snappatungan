c086eca0371905246e4863f4994bd0e137ffce1d
Deft Valian Exanova <deftvalian2411@gmail.com>
2026-06-11 21:39:44 +0700

    UPDATE: route

--- src/app/api/extract/route.ts
+++ src/app/api/extract/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+export const runtime = "edge";
+export const maxDuration = 60;
+
 // Inisialisasi Gemini
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
 
